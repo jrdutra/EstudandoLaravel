fd8d7801849fb62bcbc655367ac34e84c79276e9
Joao Ricardo Dutra <jrdutra.com.br@gmail.com>
2020-09-17 11:07:32 -0300

    Passos-GitHub

---
+++ professorGuilherme/social/src/pages/home/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <h2>Social</h2>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
