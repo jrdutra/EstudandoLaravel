50f87cc44afc158cdbb39437d44a5d69e3fdecba
jrdutra <jrdutra.com.br@gmail.com>
2020-09-18 13:08:15 -0300

    Passos-GitHub

--- professorGuilherme/social/src/pages/home/Home.vue
+++ professorGuilherme/social/src/pages/home/Home.vue
@@ -1,22 +1,39 @@
 <template>
   <div>
     <h2>SOCIAL</h2>
-    <a class="waves-effect waves-light btn"><i class="material-icons left">cloud</i>button</a>
+    <a class="waves-effect waves-light btn"
+      ><i class="material-icons left">cloud</i>button</a
+    >
+
+    <div class="row">
+      <div class="col s12 m6">
+        <div class="card blue-grey darken-1">
+          <div class="card-content white-text">
+            <span class="card-title">Card Title</span>
+            <p>
+              I am a very simple card. I am good at containing small bits of
+              information. I am convenient because I require little markup to
+              use effectively.
+            </p>
+          </div>
+          <div class="card-action">
+            <a href="#">This is a link</a>
+            <a href="#">This is a link</a>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Home',
-  data () {
-    return {
-
-    }
+  name: "Home",
+  data() {
+    return {};
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
-</style>
+<style scoped></style>
