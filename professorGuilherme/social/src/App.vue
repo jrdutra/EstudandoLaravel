4a6e1d94fca24bafdd29c379b21b00ce4cc04dff
jrdutra <jrdutra.com.br@gmail.com>
2020-09-19 09:45:28 -0300

    Passos-GitHub

--- professorGuilherme/social/src/App.vue
+++ professorGuilherme/social/src/App.vue
@@ -1,15 +1,33 @@
 <template>
-  <div>
-    <router-view/>
-  </div>
+  <span>
+    <header>
+      <nav-bar cor="green darken-1" logo="Social" url="#" />
+    </header>
+    <main>
+      <div class="container">
+        <router-view />
+      </div>
+    </main>
+    <footer-vue ano="2020" cor="green darken-1" logo="Social" descricao="Teste de descrição">
+      <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+      <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+      <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+      <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+    </footer-vue>
+  </span>
 </template>
 
 <script>
+import NavBar from "@/components/layouts/NavBar";
+import FooterVue from "@/components/layouts/FooterVue";
+
 export default {
-  name: 'App'
-}
+  name: "App",
+  components: {
+    NavBar,
+    FooterVue
+  }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
