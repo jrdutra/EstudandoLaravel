4e5bcf7115bbcc8cdb578a5981ecd1b21e0e0f87
jrdutra <jrdutra.com.br@gmail.com>
2020-09-19 10:51:55 -0300

    Passos-GitHub

--- professorGuilherme/social/src/App.vue
+++ professorGuilherme/social/src/App.vue
@@ -5,10 +5,40 @@
     </header>
     <main>
       <div class="container">
-        <router-view />
+        <div class="row">
+          <grid-vue tamanho="4">
+            <card-menu-vue>
+              <div class="row valign-wrapper">
+                <div class="col s4">
+                  <img
+                    src="https://materializecss.com/images/yuna.jpg"
+                    alt=""
+                    class="circle responsive-img"
+                  />
+                  <!-- notice the "circle" class -->
+                </div>
+                <div class="col s8">
+                  <h5>Maria Silva</h5>
+                  <span class="black-text">
+                    Descrição do perfil do usuário no sistema de teste da rede
+                    social
+                  </span>
+                </div>
+              </div>
+            </card-menu-vue>
+          </grid-vue>
+          <grid-vue tamanho="8">
+            <router-view />
+          </grid-vue>
+        </div>
       </div>
     </main>
-    <footer-vue ano="2020" cor="green darken-1" logo="Social" descricao="Teste de descrição">
+    <footer-vue
+      ano="2020"
+      cor="green darken-1"
+      logo="Social"
+      descricao="Teste de descrição"
+    >
       <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
       <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
       <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
@@ -20,12 +50,16 @@
 <script>
 import NavBar from "@/components/layouts/NavBar";
 import FooterVue from "@/components/layouts/FooterVue";
+import GridVue from "@/components/layouts/GridVue";
+import CardMenuVue from "@/components/layouts/CardMenuVue";
 
 export default {
   name: "App",
   components: {
     NavBar,
-    FooterVue
+    FooterVue,
+    GridVue,
+    CardMenuVue
   }
 };
 </script>
