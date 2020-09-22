bf8527769cddddcf1817739c993ad0611eae07d6
jrdutra <jrdutra.com.br@gmail.com>
2020-09-22 14:16:08 -0300

    Passos-GitHub

--- professorGuilherme/webservice/routes/api.php
+++ professorGuilherme/webservice/routes/api.php
@@ -1,18 +1,39 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\User;
+use Illuminate\Support\Facades\Validator;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::post('/cadastro', function (Request $request) {
+    $data = $request->all();
+
+    $validacao = Validator::make($data, [
+        'name' => ['required', 'string', 'max:255'],
+        'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+        'password' => ['required', 'string', 'min:6', 'confirmed'],
+    ]);
+
+    if($validacao->fails()){
+
+        return $validacao->errors();
+
+    }
+
+    $user = User::create(
+        [
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]
+    );
+
+    $user->token = $user->createToken($user->email)->accessToken;
+
+    return $user;
+});
+
+Route::middleware('auth:api')->get('/usuario', function (Request $request) {
     return $request->user();
 });
